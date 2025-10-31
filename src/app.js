a584322fac0b798a46a0d523b981c8ba6df54bde
Jessy-Espinoza <jessy_2388@yahoo.es>
2025-10-31 15:06:07 +0000

    GeneradorDeCartasAleatoriasJessy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardValor = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  const cardPalo = ["♦", "♥", "♠", "♣"];
+
+  const elementsPalos = document.querySelectorAll(".palo");
+  const elementCardValor = document.querySelector("#cardValor");
+  const reloadButton = document.getElementById("reloadButton");
+
+  const generarCarta = () => {
+    const valorAleatorio = cardValor[Math.floor(Math.random() * cardValor.length)];
+    const paloAleatorio = cardPalo[Math.floor(Math.random() * cardPalo.length)];
+
+    elementCardValor.innerHTML = `<p>${valorAleatorio}</p>`;
+
+    elementsPalos.forEach(el => {
+      el.innerHTML =
+        paloAleatorio === "♥" || paloAleatorio === "♦"
+          ? `<p class="text-danger">${paloAleatorio}</p>`
+          : `<p>${paloAleatorio}</p>`;
+    });
+  };
+
+  reloadButton.addEventListener("click", generarCarta);
+
+  generarCarta(); // genera una carta al cargar
 };
